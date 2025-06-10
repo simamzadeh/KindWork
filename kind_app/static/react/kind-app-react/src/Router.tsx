9bf3004cf33084d11835432e0ccf857e7ef3caea
Sima M Zadeh <77807196+simamzadeh@users.noreply.github.com>
2025-06-10 11:34:50 +0100

    Set kudos page as home page

--- kind_app/static/react/kind-app-react/src/Router.tsx
+++ kind_app/static/react/kind-app-react/src/Router.tsx
@@ -7,6 +7,10 @@ import HighlightPage from './pages/HighlightPage';
 // Login, logout and registration pages are not included in the Router as they use Bootstrap instead
 const Router = createBrowserRouter([
     {
+        path: "/",
+        element: <KudosPage />
+    },
+    {
         path: "kudos/",
         element:<KudosPage />
     },
